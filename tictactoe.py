be6f03ec4a68913028796a64822d03f64f53ba70
Jayant Gautam <jayantgautam1208@gmail.com>
2026-05-05 23:51:55 +0200

    Restore main game logic and player prompts

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,7 @@ file = open("gamerule.txt", "r")
 content = file.read()
 print(content)
 file.close()
-input("press enter to start the game")
+input("press enter to start the game").  ejeej
 
 #board range
 board = [' ' for _ in range(9)]
